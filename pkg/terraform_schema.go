044de70128d1d66a2d6f22d7971cbf16e25ebee3
magodo <wztdyl@sina.com>
2020-09-25 15:21:39 +0800

    generate tfschema scaffold from raw provider schema

--- pkg/terraform_schema.go
+++ pkg/terraform_schema.go
@@ -2,6 +2,11 @@ package pkg
 
 import "github.com/zclconf/go-cty/cty"
 
+type TerraformProviderSchemas struct {
+	FormatVersion string                       `json:"format_version"`
+	Schemas       map[string]TerraformProvider `json:"provider_schemas"`
+}
+
 type TerraformProvider struct {
 	ResourceSchemas   map[string]*TerraformSchema `json:"resource_schemas,omitempty"`
 	DataSourceSchemas map[string]*TerraformSchema `json:"data_source_schemas,omitempty"`
