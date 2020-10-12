90de8a9810e8f9ae3b0d1d9d2b59f34f52dc85d5
magodo <wztdyl@sina.com>
2020-10-12 10:45:10 +0800

    use abs path to avoid windows mixed path sep

--- cmd/cli/swgschema_azure.go
+++ cmd/cli/swgschema_azure.go
@@ -173,6 +173,8 @@ func (swgrps SWGResourceProviders) CompleteSWGResourceProvidersViaLocalFS(swagge
 			schemaPattern := regexp.MustCompile(fmt.Sprintf(`^%[1]s%[3]sresource-manager%[3]sMicrosoft.\w+%[3]s(preview|stable)%[3]s%[2]s%[3]s\w+.json$`, rpName, apiName, regexp.QuoteMeta(string(os.PathSeparator))))
 			if err := filepath.Walk(path.Join(swaggerRepoSpecBasePath, rpName),
 				func(p string, info os.FileInfo, err error) error {
+					p, _ = filepath.Abs(p)
+					swaggerRepoSpecBasePath, _ = filepath.Abs(swaggerRepoSpecBasePath)
 					relPath := strings.TrimPrefix(p, swaggerRepoSpecBasePath+string(os.PathSeparator))
 					log.Printf("Searching Swaggers in %s...\n", relPath)
 					if err != nil {
